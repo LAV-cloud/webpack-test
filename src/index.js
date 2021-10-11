84a6888135b1de6348e36588093b8cd03db1fd5c
LAV-cloud <EDSSG1337@outlook.com>
2021-10-11 21:32:06 +0700

    js html css img

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Post from './Post';
 // import json from './assets/json';
+// import WebpackLogo from './assets/.png'
 import './styles/styles.css';
 
 const post = new Post('Webpack title');
